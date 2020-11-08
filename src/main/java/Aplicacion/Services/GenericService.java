1c1cd9fe8b998ddbb0062d253005d814c5098f1a
Marllaest2075 <Marllaest2075@gmail.com>
2020-11-08 12:36:03 -0700

    Creacion de BaseModel, ContratoGenerico de Servicio, Implementacion Generica de Servicio, Modelo de Persona, Y Servicio de Persona Con su respectivo Test

---
+++ src/main/java/Aplicacion/Services/GenericService.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplicacion.Services;
+
+import Aplicacion.Models.BaseModel;
+import Interfaces.IGenericService;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marty
+ */
+public class GenericService<T extends BaseModel> implements IGenericService {
+
+    protected T _item;
+    protected ArrayList<T> _items;
+    protected String _error;
+
+    @Override
+    public ArrayList GetAll() {
+        try {
+            if (_items.isEmpty()) {
+                _error = "No hay Registros";
+                return null;
+            }
+        } catch (Exception e) {
+        }
+        return _items;
+    }
+
+    @Override
+    public BaseModel GetSingle(int id) {
+        _item = null;
+        for (int i = 0; i < _items.size(); i++) {
+            if (_items.get(i).getId() == id) {
+                _item = _items.get(i);
+                break;
+            }
+        }
+        if (_item == null) {
+            _error = "Elemento no encontrado";
+        }
+        return _item;
+    }
+
+    @Override
+    public boolean Add(BaseModel item) {
+        boolean res = false;
+        try {
+            _items.add((T) item);
+            res = true;
+        } catch (Exception e) {
+            _error = e.getMessage();
+            res = false;
+        }
+        return res;
+    }
+
+    @Override
+    public boolean Update(BaseModel item) {
+        boolean res = false;
+        try {
+            for (int i = 0; i < _items.size(); i++) {
+                if(_items.get(i).getId()== item.getId()){
+                    _items.set(i, (T) item);
+                    res = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            res = false;
+            _error = e.getMessage();
+        }
+        return res;
+    }
+
+    @Override
+    public boolean Remove(int id) {
+        boolean res = false;
+        try {
+            for (int i = 0; i < _items.size(); i++) {
+                if(_items.get(i).getId()== id){
+                    _items.remove(i);
+                    res = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            res = false;
+            _error = e.getMessage();
+        }
+        return res; 
+    }
+
+    @Override
+    public String GetError() {
+        return  _error;
+    }
+
+}
---
+++ src/main/java/Aplicacion/Services/PersonService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplicacion.Services;
+
+import Aplicacion.Models.Person;
+import Interfaces.IGenericService;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Marty
+ */
+public class PersonService extends GenericService<Person> implements IGenericService {
+    //private final ArrayList<Person> people;
+
+    public PersonService() {
+        _items = new ArrayList<>();
+        _items.add(new Person(1,"Martin","Llamas","45","Masculino",180));
+        _items.add(new Person(2,"Amanda","Servin","42","Femeino",178));
+        _items.add(new Person(3,"Jose","Llamas","20","Masculino",165));
+        _items.add(new Person(4,"Cielo","Llamas","19","Femeino",170));
+        _items.add(new Person(5,"Martincito","Llamas","16","Masculino",175));
+        
+    }
+    
+   
+    
+}
