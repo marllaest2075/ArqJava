06d7fa5e005316e4c28572dcb6793d9d37d9c856
Marllaest2075 <Marllaest2075@gmail.com>
2020-11-08 12:37:26 -0700

    Creacion de BaseModel, ContratoGenerico de Servicio, Implementacion Generica de Servicio, Modelo de Persona, Y Servicio de Persona Con su respectivo Test

--- src/main/java/Aplicacion/App.java
+++ src/main/java/Aplicacion/App.java
@@ -1,5 +1,7 @@
 package Aplicacion;
 
+import Aplicacion.Views.MainView;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new MainView().show();
     }
 }
