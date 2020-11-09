6dc226479a0a0fe736b24efb430f40f7796237fa
Marllaest2075 <Marllaest2075@gmail.com>
2020-11-08 18:06:49 -0700

    Vista de People lista

---
+++ src/main/java/Aplicacion/Constantes/Movimiento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplicacion.Constantes;
+
+/**
+ *
+ * @author Marty
+ */
+public enum Movimiento {
+    None,
+    Nuevo,
+    Actualizar,
+    Eliminar
+}
